528350bbbacc7b335972a1590f394391e35a284f
Richard Stock <rstock.co@gmail.com>
2018-09-17 19:01:14 -0600

    I made changes

---
+++ assign_2_v2.js
@@ -0,0 +1,26 @@
+function flip_string_v2 (sentence) {
+    let flipped_sentence = "";
+
+    for (let i = sentence.length - 1; i >= 0; i--) {
+        flipped_sentence = flipped_sentence + sentence[i];
+    }
+    return flipped_sentence;
+}
+const flip_string_v3 = (sentence) => {
+    let flipped_sentence = ""
+
+    for (let i = sentence.length - 1; i >= 0; i--) {
+        flipped_sentence = flipped_sentence + sentence[i]
+    }
+    return flipped_sentence
+}
+
+const flip_sentence = (sentence) => {
+    
+}
+
+ console.log(flip_string_v2("?A nut for a jar of tuna?"));
+ console.log(flip_string_v3("?A nut for a jar of tuna?"));
+        
+
+ 
