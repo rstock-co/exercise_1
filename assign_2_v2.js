bee2be389292bdd127809fc29455ff3504e4f939
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2018-09-17 19:22:01 -0600

    Updated the code with a comment

--- assign_2_v2.js
+++ assign_2_v2.js
@@ -1,26 +1,22 @@
-function flip_string_v2 (sentence) {
-    let flipped_sentence = "";
+//Just checking out your code
+function flip_string_v2(sentence) {
+  let flipped_sentence = ''
 
-    for (let i = sentence.length - 1; i >= 0; i--) {
-        flipped_sentence = flipped_sentence + sentence[i];
-    }
-    return flipped_sentence;
+  for (let i = sentence.length - 1; i >= 0; i--) {
+    flipped_sentence = flipped_sentence + sentence[i]
+  }
+  return flipped_sentence
 }
-const flip_string_v3 = (sentence) => {
-    let flipped_sentence = ""
+const flip_string_v3 = sentence => {
+  let flipped_sentence = ''
 
-    for (let i = sentence.length - 1; i >= 0; i--) {
-        flipped_sentence = flipped_sentence + sentence[i]
-    }
-    return flipped_sentence
-}
-
-const flip_sentence = (sentence) => {
-    
+  for (let i = sentence.length - 1; i >= 0; i--) {
+    flipped_sentence = flipped_sentence + sentence[i]
+  }
+  return flipped_sentence
 }
 
- console.log(flip_string_v2("?A nut for a jar of tuna?"));
- console.log(flip_string_v3("?A nut for a jar of tuna?"));
-        
+const flip_sentence = sentence => {}
 
- 
+console.log(flip_string_v2('?A nut for a jar of tuna?'))
+console.log(flip_string_v3('?A nut for a jar of tuna?'))
